3544c8a7c29dcb5a621ef91439a64a1e7e150205
Samy Kamal <samy.kamal@noaa.gov>
2022-01-21 17:45:37 +0000

    WCOSS2 first commit

---
+++ sorc/build_st4_mrms_hrlywgts.sh
@@ -0,0 +1,28 @@
+set -x
+
+##############################
+
+BASE=$(pwd)
+export BASE
+
+set -x
+. /usr/share/lmod/lmod/init/ksh
+module purge
+module load envvar/1.0
+module load PrgEnv-intel/8.1.0
+module load craype/2.7.8
+module load intel/19.1.3.304
+#module load cray-mpich/8.1.9
+module use ../modulefiles
+module load PCPANL
+
+module list
+
+cd ${BASE}/st4_mrms_hrlywgts.fd
+make clean
+make
+make mvexec
+make clean
+
+##############################
+
